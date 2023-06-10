bb0691f4d7a0a40231282d59909d419c9ca629fa
Razvan Constantin <razvan.constantin@nmi.com>
2023-06-10 13:02:39 +0100

    Add files

---
+++ FishTank/FishTypes/GoldFish.cs
@@ -0,0 +1,10 @@
+﻿namespace FishTank
+{
+    public class GoldFish : Fish
+    {
+        public GoldFish(string name) : base(name)
+        {
+            RequiredFood = (decimal)0.1;
+        }
+    }
+}
---
+++ FishTank/Tank.cs
@@ -0,0 +1,29 @@
+﻿namespace FishTank
+{
+    public class Tank
+    {
+        public Fish[]? FishList;
+
+        public void AddFish(Fish[] list)
+        {
+            FishList = list;
+        }
+
+        public decimal Feed()
+        {
+            decimal totalFood = 0;
+
+            if (FishList == null)
+            {
+                return totalFood;
+            }
+
+            foreach (Fish fish in FishList)
+            {
+                totalFood += fish.RequiredFood;
+            }
+
+            return totalFood;
+        }
+    }
+}
